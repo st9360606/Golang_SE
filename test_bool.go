fe18a7310cbedbe8320791190c5daff364e6534c
kurt-jyint <kurt.wu@jyintgroup.com>
2023-05-22 11:57:31 +0800

    learn datatype

---
+++ test_bool.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	var b1 bool = true
+	var b2 bool = false
+	var b3 = true
+	var b4 = false
+	b5 := true
+	b6 := false
+
+	fmt.Printf("b1: %v\n", b1)
+	fmt.Printf("b2: %v\n", b2)
+	fmt.Printf("b3: %v\n", b3)
+	fmt.Printf("b4: %v\n", b4)
+	fmt.Printf("b5: %v\n", b5)
+	fmt.Printf("b6: %v\n", b6)
+
+	//if
+	age := 18
+	if age >= 18 {
+		fmt.Println("已成年...")
+	} else {
+		fmt.Println("未成年...")
+	}
+
+	//for
+	count := 10
+	for i := 0; i < count; i++ {
+		fmt.Printf("i: %v\n", i)
+	}
+
+	age1 := 20
+	gender := "男"
+	if age1 >= 18 && gender == "男" {
+		fmt.Println("你是成年男子")
+	}
+
+}
---
+++ test_datatype.go
@@ -0,0 +1,29 @@
+package main
+
+// import "fmt"
+
+// func a() {
+
+// }
+
+// func main() {
+// 	fmt.Printf("%T\n", a)
+
+// 	var name string = "tom"
+// 	age := 20
+// 	b := true
+// 	fmt.Printf("%T\n", name)
+// 	fmt.Printf("%T\n", age)
+// 	fmt.Printf("%T\n", b)
+
+// 	a := 100
+// 	p := &a
+// 	fmt.Printf("%T\n", p)
+
+// 	c := [2]int{1, 2}
+// 	fmt.Printf("%T\n", c)
+
+// 	D := []int{1, 2, 3} //切片類型
+// 	fmt.Printf("%T\n", D)
+
+// }
