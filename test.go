3371505e6d3715b73e99be3bd3576b3d11960851
kurt-jyint <kurt.wu@jyintgroup.com>
2023-05-22 10:37:03 +0800

    上傳檔案

---
+++ test.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func getNameAndAge()(string, int){
+	return "tom",20
+}
+
+func main() {
+	//匿名輸出
+	_,age:=getNameAndAge()
+	// fmt.Printf("name: %v\n", name)
+	fmt.Printf("age: %v\n", age)
+
+	// var name string
+	// var age int
+	// var  m bool
+
+	//批量聲明
+	// var (
+	// 	name string
+	// 	age  int
+	// 	b    bool
+	// )
+	// name="tom"
+	// age = 20
+	// b= true
+	// fmt.Printf("name: %v\n", name)
+	// fmt.Printf("age: %v\n", age)
+	// fmt.Printf("b: %v\n", b)
+
+	// //類型推斷
+	// var name1 = "ok"
+	// fmt.Printf("name1: %v\n", name1)
+
+	// //批量初始化
+	// var name,age,b ="cindy",10,false
+	// fmt.Printf("name: %v\n", name)
+	// fmt.Printf("age: %v\n", age)
+	// fmt.Printf("b: %v\n", b)
+
+	//短變量聲明
+	skill:= "golang"
+	fmt.Printf("skill: %v\n", skill)
+}
