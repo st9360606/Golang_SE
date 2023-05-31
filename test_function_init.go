c8ccca8140b19e804b388df2ee66b69541bcefb1
kurt-jyint <kurt.wu@jyintgroup.com>
2023-05-31 14:51:28 +0800

    fix name

---
+++ test_function_init.go
@@ -0,0 +1,24 @@
+package main
+
+// import "fmt"
+
+// var i = initVar()
+
+//快捷鍵 finit
+// func init() {
+// 	fmt.Println("init2......")
+// }
+
+// func init() {
+// 	fmt.Println("init......")
+// }
+
+// func initVar() int {
+// 	fmt.Println("initVar.....")
+// 	return 100
+// }
+
+// func main() {
+// 	//初始化順序:變量初始化 -> init() -> main()
+// 	fmt.Println("main.....")
+// }
